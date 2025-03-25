d3aee079f69779264cf2d98b0ed31c0b526b8981
Emilia Paz <167714900+EmiliaPazArredondo@users.noreply.github.com>
2025-03-25 12:36:24 -0300

    Add files via upload

---
+++ monedas.py
@@ -0,0 +1,10 @@
+# Emilia arredondo contreras
+# miau
+# 25-03-2025
+
+pesos = int(input("cuanto te queda en pesos colombianos?"))
+soles = int(input("cuanto te queda en pesos peruanos?"))
+reales = int(input("cuanto te queda en pesos brasil?"))
+
+total = pesos * 0.00025 + soles * 0.28 + reales * 0.21
+print (total)
---
+++ temperatura.py
@@ -0,0 +1,9 @@
+# Emilia arredondo
+# 25-03-2025
+# Convertidor de grados celsius a farenheit
+
+temp_f = 59
+
+temp_c = (temp_f - 32)/1.8
+
+print(temp_c)
