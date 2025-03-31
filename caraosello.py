d7a1701b3a627f7032b6a6b386e072125d17e883
Emilia Paz <167714900+EmiliaPazArredondo@users.noreply.github.com>
2025-03-31 12:22:54 -0300

    Add files via upload

---
+++ caraosello.py
@@ -0,0 +1,13 @@
+#Emilia Arredondo
+#31-03-2025
+#una vez me esguincé los dos tobillos
+
+import random
+
+num = random.randint(0, 1)
+
+if num > 0.5 :
+    print("cara")
+
+else:
+    print("sello")
---
+++ promedio.py
@@ -0,0 +1,10 @@
+# Emilia Arredondo Contreras
+# 31-03-2025
+
+nota = 70
+
+if nota >= 40:
+    print("tu pasaste")
+
+else:
+    print("No pasaste")
