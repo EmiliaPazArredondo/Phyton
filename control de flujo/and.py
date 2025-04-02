2b3b6b1f3e8c7ef8bdb0ef6b3cd7bb7676b234f5
Emilia Arredondo <167714900+EmiliaPazArredondo@users.noreply.github.com>
2025-04-02 11:07:13 -0300

    Add files via upload

---
+++ control de flujo/and.py
@@ -0,0 +1,9 @@
+#Emilia Arredondo
+#02-04-2025
+
+usuario = input("ingresar tu nombre")
+hambre = int(input("¿cuánta hambre tienes? (1-10)"))
+enojo = int(input("¿Cuánto enojo tienes? (1-10)"))
+
+if hambre > 4 and enojo > 1:
+    print("Enfadado por hambre")
