5b6e8b3417347e3239e766d174714d591fa7c29b
Emilia Arredondo <167714900+EmiliaPazArredondo@users.noreply.github.com>
2025-04-03 08:20:59 -0300

    Add files via upload

---
+++ control de flujo/or.py
@@ -0,0 +1,14 @@
+#Emilia Arredondo 
+#02-04-2025
+# me gané un completito 
+
+print("¡Bienvenido a tu día feliz!")
+
+cafe = int(input("¿Cúantas tazas de café has tomado hoy? (0 o más)"))
+té_burbujas = int(input("¿cúantas tazas de té de burbujas haz tomado hoy? (0 o más)"))
+
+if cafe >0 or té_burbujas >0:
+    print("¡Que bien! Haz disfrutado de algo rico hoy")
+
+else:
+    print("Parece que no has tomado nada especial hoy :( ¡Anímate!") 
